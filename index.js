0c8d8876f6f40138283f68b5be9212f33b0a9611
rznayeem <riyazulnayeem@gmail.com>
2024-04-26 02:24:28 +0600

    user collection api added for post userdata on mongodb

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db('tourTrekkerDB').collection('user');
+
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
